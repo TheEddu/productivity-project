bd1ba6c443a059fcd032ff6461676f93abc93871
TheEddu <edsan42@protonmail.com>
2025-08-18 11:36:00 -0300

    Adição de scroll e permissão de barra (/) para Lista

--- scripts/list.js
+++ scripts/list.js
@@ -14,7 +14,7 @@ const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
 const removeSpecialChars = (val) => {
-  return val.trim().replace(/[^A-Za-z0-9À-ÿçÇ\-:\s,\.]/g, '')
+  return val.trim().replace(/[^A-Za-z0-9À-ÿçÇ\-:\s,\.\/]/g, '')
 }
 
 const addOrUpdateTask = () => {
